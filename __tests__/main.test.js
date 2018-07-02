0584fa75f55c769776aa40d7b2ad40b81a8a7266
imcuttle <moyuyc95@gmail.com>
2018-07-02 13:15:29 +0800

    tests: remove

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -9,5 +9,7 @@ const { readFileSync, writeFileSync } = require('fs')
 const { makeFixture } = require('./helper')
 
 describe('main', function() {
-
+  it('should ok', function () {
+    expect(1).toBe(1)
+  })
 })
