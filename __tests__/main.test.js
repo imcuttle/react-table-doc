11b9d9e8656b4cedb059cc620c645721013d7782
imcuttle <moyuyc95@gmail.com>
2018-07-02 13:15:04 +0800

    tests: remove

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -9,18 +9,5 @@ const { readFileSync, writeFileSync } = require('fs')
 const { makeFixture } = require('./helper')
 
 describe('main', function() {
-  beforeAll(function () {
-    writeFileSync(
-      makeFixture('button.json'),
-      JSON.stringify(
-        parse(readFileSync(makeFixture('Button.js')).toString()),
-        null,
-        2
-      )
-    )
-  })
 
-  it('should react-docgen passed', function() {
-    expect(require(makeFixture('button.json'))).toMatchSnapshot()
-  })
 })
